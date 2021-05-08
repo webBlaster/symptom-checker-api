0df397f156f7357e18fa45893fce2ccfe4b66574
Yekinni Ajibola <yekinnijibola@gmail.com>
2021-05-08 18:09:03 +0100

    made port dynamic

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ app.get("/getSymptoms/:string", (req, res) => {
   res.json(result);
 });
 
-app.listen(3000, () => {
-  console.log("App is listening on port 3000 ....");
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`App is Listening on port ${port}`);
 });
